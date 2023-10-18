43c6056dd027a83027463d3cc109ec97983ae5f5
John Cotton <john@dynamicarray.co.uk>
2023-10-18 18:06:28 +0100

    Quick upload tool

--- up-assets.bat
+++ up-assets.bat
@@ -1,2 +1,5 @@
 aws s3 sync d:\Dropbox\development\genie-marketing-hub-master\GenieHub\genie-hub-cloud\public\_assets s3://genie-hub-2/_assets/ --delete --cache-control max-age=3600
 aws s3 sync d:\Dropbox\development\genie-marketing-hub-master\GenieHub\genie-hub-cloud\public\_assets s3://genie-cloud/_assets/ --profile genie-hub-active --delete --cache-control max-age=3600
+
+aws s3 sync d:\Dropbox\development\genie-marketing-hub-master\GenieHub\genie-hub-cloud\public\genie-tools s3://genie-hub-2/genie-tools/ --cache-control max-age=3600
+aws s3 sync d:\Dropbox\development\genie-marketing-hub-master\GenieHub\genie-hub-cloud\public\genie-tools s3://genie-cloud/genie-tools/ --profile genie-hub-active --cache-control max-age=3600
