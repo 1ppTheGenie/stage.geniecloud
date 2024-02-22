304764a0dce198f3a7166b0a67d04c177cf4a25e
John Cotton <john@dynamicarray.co.uk>
2024-02-22 23:46:57 +0000

    Add "alternating" theme hues for embeds

--- genie-components/src/landing/_LandingPages.jsx
+++ genie-components/src/landing/_LandingPages.jsx
@@ -554,6 +554,10 @@ export default () => {
 				});
 			});
 		}
+	} );
+	
+	document.querySelectorAll(`.genie-alternate:nth-child(odd)`).forEach(el => {
+		el.classList.add( window.gHub.alternateShade??'dark' );
 	});
 
 	window.requestAnimationFrame(() => window.gHub.lazyLoader("img.lazy"));
