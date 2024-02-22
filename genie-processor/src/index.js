448d80989eb4d2c6800be4cc5d2fb1381f9e2371
John Cotton <john@dynamicarray.co.uk>
2024-02-22 23:24:10 +0000

    Make local testing of asset changes easier!

--- genie-processor/src/index.js
+++ genie-processor/src/index.js
@@ -1,3 +1,6 @@
+import http from "http";
+import url  from 'url';
+import querystring from 'querystring';
 import fs from "fs";
 import { basename } from "path";
 import SaxonJS from "saxon-js/SaxonJS2N.js";
@@ -5,24 +8,18 @@ import SaxonJS from "saxon-js/SaxonJS2N.js";
 // prettier-ignore
 import { S3Client,DeleteObjectCommand, GetObjectCommand, PutObjectCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
+const JSON_MIME = "application/json";
 const REGION = process.env.REGION ?? "eu-west-2";
 const BUCKET = process.env.BUCKET ?? "genie-hub-2";
-const GENIE_URL =
-	process.env.GENIE_URL ?? "https://genie-hub-2.s3.eu-west-2.amazonaws.com/";
+const GENIE_URL = process.env.GENIE_URL ?? "https://genie-hub-2.s3.eu-west-2.amazonaws.com/";
 
-const JSON_MIME = "application/json";
-
-const KEEP_RENDER_FOR = process.env?.KEEP_RENDER_FOR || 52;
-const WEEK_IN_MILLISECONDS = 60 * 60 * 24 * 1000;
-
-// Set up the S3 client
 const s3Client = new S3Client({ region: REGION });
 
 const TEMP_DIR =
 	process.env.TEMP_DIR ??
 	"D:/Dropbox/development/genie-marketing-hub-master/GenieHub/genie-hub-cloud/public/_assets/_xsl_imports/";
 
-const transform = (xml, xslt, xsltBaseUri, method='xml') => {
+const transform = (xml, xslt, xsltBaseUri, method = "xml") => {
 	try {
 		const result = SaxonJS.XPath.evaluate(
 			`
@@ -55,7 +52,7 @@ const copyFilesToLocal = async () => {
 	// Files imported via xsl:import have to exist on a "local" drive
 	if (
 		TEMP_DIR !==
-		"D:/Dropbox/development/genie-marketing-hub-master/GenieHub/genie-hub-cloud/public/_assets/_xsl_imports/" // &&!fs.existsSync(iconsPath) might want this on there to avoid doing this each time
+		"D:/Dropbox/development/genie-marketing-hub-master/GenieHub/genie-hub-cloud/public/_assets/_xsl_imports/"
 	) {
 		const imports = await listS3Folder("_assets/_xsl_imports/");
 		await Promise.all(
@@ -124,7 +121,11 @@ export const xslt = async event => {
 							await toS3(
 								s3Target,
 								transformedXML,
-								{ finalRender: true, ...params.tags,"Genie-Delete": "extended" },
+								{
+									finalRender: true,
+									...params.tags,
+									"Genie-Delete": "extended",
+								},
 								"text/html"
 							);
 						} else {
@@ -255,65 +256,61 @@ const toS3 = async (
 	} catch (err) {}
 };
 
-const testXSL = async testKey => {
-	const transformXsl = (await fromS3(`_assets/_xsl/${testKey}.xsl`)).toString();
-	const transformXml = (
-		await fromS3("_assets/_reference/_genie-sample.xml")
-	).toString();
 
-	const r = transform(transformXml, transformXsl, `file://${TEMP_DIR}`);
-
-	console.log(r);
-};
-/*
-await testXSL("direct-mail-postcards-back/coit-mailer-back");
-
-let r = await xslt({
-	Records: [
-		{
-			s3: {
-				object: {
-					key: "_processing/56947122-946d-429e-b39c-32cb0b32a45a/lc-prop-post-03a-png-p0-xslt.json",
-				},
-			},
-		},
-	],
-});
-
-const promises = r.map(async s3 => {
+if ( process.argv.length > 2 ) {
 	try {
-		let transformXsl;
-		try {
-			let buffer,
-				i = 0;
-			while (!buffer && i <= 4) {
-				buffer = await fromS3(s3.Key);
-				if (typeof buffer !== "undefined") {
-					transformXsl = buffer.toString();
-					i = 6;
-				} else {
-					i++;
-				}
-			}
+		if ( process.argv[2] == "test" ) {
+			const assetDir = process.cwd() + '/../public/_assets/_xsl/';
+			const dataDir = process.cwd() +'/../public/_assets/_reference/';
 
-			if (i != 6) console.log("Failed on:", s3.Key);
-		} catch (e) {
-			console.log("Nope:", s3.Key.replace(`_assets/_xsl/`, ""), e);
-		}
-		if (transformXsl) {
-			const r = transform(transformXml, transformXsl, `file://${TEMP_DIR}`);
+			const server = http.createServer( async ( req, res ) => {
+				const parsedUrl = url.parse(req.url);
+				const queryParams = querystring.parse(parsedUrl.query);		
+				
+				const assetPath = `${assetDir}${queryParams.xsl}.xsl`;
+				const dataPath = `${dataDir}${queryParams?.xml ?? '_genie-sample'}.xml`;
+					
+				if ( !fs.existsSync( assetPath ) ) {
+					res.writeHead( 200, { "Content-Type": "text/plain" } );
+					res.end( `Asset ${queryParams.xsl} does not exist` );
+					return;					
+				} 
 
-			if (typeof r == "object" && r.failed) {
-				console.log(s3.Key.replace(`_assets/_xsl/`, ""), "\t", r.msg);
-			}
+				if ( !fs.existsSync( dataPath ) ) {
+					res.writeHead( 200, { "Content-Type": "text/plain" } );
+					res.end( `Data file ${dataPath} does not exist` );
+					return;					
+				} 
+
+				const output = await testXSL( assetPath, dataPath, null );
+				
+				res.writeHead( 200, { "Content-Type": "text/html" } );
+				res.end( typeof output == 'string' ? output: JSON.stringify(output) );
+			} );
+
+			const PORT = process.env.PORT || 3000;
+			
+			server.listen(PORT, () => {
+				console.log(`Server is running on port ${PORT}`);
+			});
 		}
-	} catch (e) {
-		console.log(s3.Key.replace(`_assets/_xsl/`, ""));
-		console.log("Failed: ", s3.Key, e);
+	} catch (err) {
+		console.log("Error:", err);
 	}
-});
-
-for (const promise of promises) {
-	await promise;
 }
-*/
+
+const testXSL = async (xslKey,  xmlKey = "_assets/_reference/_genie-sample.xml", outName = "output.svg") => {
+	const transformXsl = fs.existsSync(xslKey) ? fs.readFileSync(xslKey, 'utf8') : ( await fromS3( `_assets/_xsl/${xslKey}.xsl` ) ).toString();
+	const transformXml = fs.existsSync(xmlKey) ? fs.readFileSync(xmlKey, 'utf8') : ( await fromS3(xmlKey) ).toString();
+
+	const r = transform( transformXml, transformXsl, `file://${TEMP_DIR}` );
+
+	if ( outName ) {
+		fs.writeFile( outName, r, err => {
+			if ( err ) throw err;
+			console.log( `Data has been written to the ${out}.` );
+		} );
+	}
+	
+	return r;
+};
