128103edab3d82fd2bdc92065225a4e5396d1841
Andrew Meyer <andrewmeyer23@gmail.com>
2024-07-30 11:33:26 -0500

    trying again

--- genie-api/src/utils/render-data.js
+++ genie-api/src/utils/render-data.js
@@ -464,9 +464,9 @@ const processAgents = async agentIds => {
                 disclaimerExtra: getDisclaimer(4).text, //"htmlDisclaimer"
                 disclaimerIDX: getDisclaimer(3).text, // "idxDisclaimer"
 
-                snippetHeadTag: getSnippet(1).codeSnippet,
-                snippetOpenBodyTag: getSnippet(2).codeSnippet,
-                snippetCloseBodyTag: getSnippet(3).codeSnippet,
+                snippetHeadTag: `<![CDATA[${getSnippet(1).codeSnippet}]]>`,
+                snippetOpenBodyTag: `<![CDATA[${getSnippet(2).codeSnippet}]]>`,
+                snippetCloseBodyTag: `<![CDATA[${getSnippet(3).codeSnippet}]]>`,
 
                 pronoun: marketingSettings.profile.isTeam
                     ? 'plural'
