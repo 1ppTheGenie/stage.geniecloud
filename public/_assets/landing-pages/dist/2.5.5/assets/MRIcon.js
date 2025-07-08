97ef838c13ea75ad39d578a583ed18df717d6a8e
John Cotton <john@dynamicarray.co.uk>
2025-07-08 09:37:11 +0100

    Build v2.5.5

---
+++ public/_assets/landing-pages/dist/2.5.5/assets/MRIcon.js
@@ -0,0 +1,2 @@
+import{i,e as c,f as v,t as r}from"./index.js";const d=r('<div class="mr-icon"><div></div><div class="avg-text"></div><div class="avg-value">'),f=e=>(()=>{const t=d(),l=t.firstChild,a=l.nextSibling,n=a.nextSibling;return i(l,()=>e.icon),i(a,()=>e.caption),i(n,()=>e.value),c(s=>v(t,e.style,s)),t})();export{f as M};
+//# sourceMappingURL=MRIcon.js.map
